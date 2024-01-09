ddb07de33ca053b9ae4525401075a9e530acb250
lpilla <lorenzo.pilla03@gmail.com>
2024-01-09 16:46:24 +0100

    feat : added foreground

--- app/src/main/java/com/example/progettobackground/services/MyService.java
+++ app/src/main/java/com/example/progettobackground/services/MyService.java
@@ -1,11 +1,16 @@
 package com.example.progettobackground.services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import com.example.progettobackground.R;
 
 public class MyService extends Service {
     private Boolean isRunning = false;
@@ -29,6 +34,18 @@ public class MyService extends Service {
                     }
                 }
         ).start();
+
+        final String CHANNEL_ID = "Foreground Service ID";
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_ID, NotificationManager.IMPORTANCE_LOW);
+
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+
+        Notification.Builder notification = new Notification.Builder(this,CHANNEL_ID)
+                .setContentText("Service is running")
+                .setContentTitle("Service enabled")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        startForeground(1001,notification.build());
 
         return super.onStartCommand(intent,flags,startId);
     }
